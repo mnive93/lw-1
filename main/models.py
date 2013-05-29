760ce3c688dc7d99f9f8c4b754ea1e314673f07c
Gokul Nath Sridhar <gokulnathsridhar@yahoo.co.in>
2013-05-29 17:31:09 +0530

    Prior to doing Twitter/Facebook stuff

--- main/models.py
+++ main/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class SignupHandler(models.Model):
     emailaddress = models.EmailField()
     identifiernum = models.IntegerField()
+    
+class Posts(models.Model):
+    user = models.ForeignKey(User)
+    text = models.CharField(max_length=250)
+    time = models.DateTimeField(auto_now_add = True)
+    popularity = models.IntegerField(default = 1)
